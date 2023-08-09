83073c5b5b1efb9df165ade4c5ed367588361b64
lazhangzheng <zhangzheng@microsoft.com>
2023-08-09 15:04:34 +0800

    test symlink dir

--- standardFlow/echo_my_prompt.py
+++ standardFlow/echo_my_prompt.py
@@ -1,5 +1,5 @@
 from promptflow import tool
-from hello import hello
+from dirlink import hello
 # The inputs section will change based on the arguments of the tool function, after you save the code
 # Adding type to arguments and return value will help the system show the types properly
 # Please update the function name/signature per need
@@ -7,5 +7,5 @@ from hello import hello
 
 @tool
 def my_python_tool(input1: str) -> str:
-    hello()
+    hello.hello()
     return "Prompt: " + input1
